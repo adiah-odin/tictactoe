0c7deb79900b1b03e07ea33a2b8a0a8710e974b5
nathanieladiah <nathanieladiah@gmail.com>
2022-06-02 14:56:36 -0400

    Add animation from title screen to gameboard

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,37 @@
+const titleScreen = document.getElementById('title');
+const gameBoard = document.getElementById('gameBoard');
+
 // Create the layout for the board
 const boardSpaces = [null, null, null, null, null, null, null, null, null];
 
-const gameBoard = document.getElementById('gameBoard')
-
-boardSpaces.forEach(space => {
+boardSpaces.forEach((space, index) => {
 	const button = document.createElement('button');
 	button.className = ('game-board__button');
 	gameBoard.append(button);
 })
+
+// Switch to the gameboard when icon is clicked
+// const playerSelectors = titleScreen.getElementsByClassName('icon');
+const playerSelectors = [...titleScreen.getElementsByClassName('icon')];
+
+playerSelectors.forEach(selector => {
+	selector.onclick = startGame;
+})
+
+function startGame() {
+	// hide the title screen
+	titleScreen.setAttribute('exit', '')
+
+	titleScreen.addEventListener('animationend', () => {
+		titleScreen.style.display = 'none';
+		titleScreen.removeAttribute('exit');
+
+		gameBoard.style.display = 'flex';
+
+	}, {once: true});
+
+
+
+	// gameBoard.style.display = 'flex';
+	// show the game-board
+}
